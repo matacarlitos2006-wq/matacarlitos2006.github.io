4a83f60119ded5dbecf0449a3aaa2a97fcc08310
matacarlitos2006-wq <matacarlitos2006@gmail.com>
2025-11-05 21:40:03 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+(function() {
+  const params = new URLSearchParams(window.location.search);
+  const funnel = params.get('funnel');
+  const leadOffer = params.get('lead_introductory_offer');
+  console.log('Funnel:', funnel);
+  console.log('Lead Offer:', leadOffer);
 
+  if (funnel === '75off' && leadOffer === '75off') {
+    // maybe show a special badge or modal
+    alert('Welcome! You have the 75% off special offer!');
+  }
+})();
